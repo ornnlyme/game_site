9d066079364988ecf6ee6a5465927bb8d3aca001
ornllyme <ornnly.now@gmail.com>
2024-11-30 21:32:23 +0700

    Add sidebar

--- assets/js/loadgame.js
+++ assets/js/loadgame.js
@@ -428,3 +428,42 @@ const showGames = function () {
 }
 init_game()
 showGames()
+
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    let game_id = ""
+    if (window.location.href.split('?').length >= 2) {
+        game_id = window.location.href.split('?')[1]
+    }
+
+    const gamesList = document.getElementById("sidebar-games-list");
+    if (!gamesList) {
+        return;
+    }
+    const promoted_games = games.filter(game => game.id !== game_id);
+    shuffle(promoted_games);
+    for (let i = 0; i < 18; i++) {
+        const promoted_game = promoted_games[i];
+        const gameHtml = `
+                        <li>
+                            <a href="game.html?${promoted_game.id}" class="as-game-box as-related-game">
+                                <div class="as-related-resize">
+                                    <picture>
+                                        <source srcset="${promoted_game.avif_game_preloader_image}" type="image/avif">
+                                        <img alt="${promoted_game.name}" class="game-image" src="${promoted_game.fallback_game_preloader_icon_image}">
+                                    </picture>
+                                </div>
+                                <span class="as-game-box-title as-related-title">${promoted_game.name}</span>
+                            </a>
+                        </li>
+                    `;
+        gamesList.insertAdjacentHTML("beforeend", gameHtml);
+    }
+
+});
