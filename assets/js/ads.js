6b61007c562aa2854013e88200fcb714103566b7
ornllyme <ornnly.now@gmail.com>
2024-12-05 16:53:59 +0700

    update ads

--- assets/js/ads.js
+++ assets/js/ads.js
@@ -1,5 +1,7 @@
 window.addEventListener("load", function () {
     // Trigger AdSense ad loading on page reload
     (adsbygoogle = window.adsbygoogle || []).push({});
+    const adContainer = document.getElementById("full-screen-ads");
+    adContainer.style.display = "block";
 });
 
